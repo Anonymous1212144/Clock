0b12d8aa862a3e8a00aaccb8d034cd49810c5c4c
Anonymous1212144 <101783700+Anonymous1212144@users.noreply.github.com>
2025-10-14 20:03:55 -0600

    Removed dependency on printf

--- clock.c
+++ clock.c
@@ -3,16 +3,19 @@
 
 int main()
 {
+    const char *format = "%F W%V-%u UTC%z     %T.000000000\r";
     struct timespec ts;
-    struct tm ltime;
     char buff[50];
+    char *b;
 
     while (1)
     {
         timespec_get(&ts, TIME_UTC);
-        localtime_s(&ltime, &ts.tv_sec);
-        strftime(buff, sizeof buff, "%F W%V-%u UTC%z      %T", &ltime);
-        printf("%s.%09ld\r", buff, ts.tv_nsec);
+        strftime(buff, sizeof buff, format, localtime(&ts.tv_sec));
+        b = buff + 47;
+        do
+            *(b--) = '0' + ts.tv_nsec % 10;
+        while (ts.tv_nsec /= 10);
+        fputs(buff, stdout);
     }
-
 }
